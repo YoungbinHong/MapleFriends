caa00022f777af4bbdc58dbdcf0f36995b1557e6
YoungbinHong <ybhong1995@gmail.com>
2025-08-06 01:15:36 +0900

    커밋커밋

---
+++ src/test/java/com/example/maplefriends/MapleFriendsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maplefriends;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapleFriendsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
